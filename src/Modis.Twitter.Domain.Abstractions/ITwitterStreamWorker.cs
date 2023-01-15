4581073b78beaef06acabb8b12fccb672881414e
rserajedinov <rserajedinov@accesssoftek.com>
2023-01-15 12:29:40 -0800

    add some summary tags

--- src/Modis.Twitter.Domain.Abstractions/ITwitterStreamWorker.cs
+++ src/Modis.Twitter.Domain.Abstractions/ITwitterStreamWorker.cs
@@ -1,5 +1,9 @@
 ﻿namespace Modis.Twitter.Domain.Abstractions;
 public interface ITwitterStreamWorker
 {
+    /// <summary>
+    /// Watches top #10 tweet hash-tags
+    /// </summary>
+    /// <param name="cts"></param>
     void WatchTwitterStream(CancellationToken cts);
 }
--- src/Modis.TwitterClient.Abstractions/ITwitterClient.cs
+++ src/Modis.TwitterClient.Abstractions/ITwitterClient.cs
@@ -2,8 +2,16 @@
 {
     using Modis.TwitterClient.Abstractions.Model;
 
+    /// <summary>
+    /// Tweeter web client
+    /// </summary>
     public interface ITwitterClient
     {
+        /// <summary>
+        /// Consume async tweets stream
+        /// </summary>
+        /// <param name="cancellationToken">A token which stops streaming</param>
+        /// <returns>Asynchronous stream of tweets</returns>
         IAsyncEnumerable<Tweet> GetTweetStream(CancellationToken cancellationToken = default);
     }
 }
